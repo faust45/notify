97c7d45b99567cd917718b1beed2649eee863e86
Anton <faust45@antons-macbook-air.local>
2009-11-05 10:32:55 +0300

    fix

--- app.rb
+++ app.rb
@@ -6,11 +6,11 @@ twitter = Twitter::Client.from_config('config.yml')
 
 #status = twitter.status(:post, 'NOT buying overrated iPhone.')
 
-watcher = DirWatcher.new('/home/ftp/ftp_data/raw')
 trap("INT") do
   watcher.down
   exit
 end
+watcher = DirWatcher.new('/home/ftp/ftp_data/raw')
 
-log 'blin'
+log 'main thread continue'
 gets
